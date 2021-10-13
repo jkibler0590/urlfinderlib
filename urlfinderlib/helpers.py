70121af8818d00eca177d3777af46b452d792c53
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-10-13 15:01:38 +0000

    Removes hidden unicode characters and fixes malformated scheme

--- urlfinderlib/helpers.py
+++ urlfinderlib/helpers.py
@@ -15,10 +15,24 @@ def fix_possible_url(value: str, domain_as_url: bool = False) -> str:
 
 def fix_possible_value(value: str) -> str:
     value = value.strip()
+    value = remove_hidden_unicode_characters(value)
     value = fix_slashes(value)
+    value = fix_scheme(value)
     value = remove_mailto_if_not_email_address(value)
     value = remove_null_characters(value)
     value = remove_surrounding_quotes(value)
+    return value
+
+
+def fix_scheme(value: str) -> str:
+    try:
+        split_value = urlsplit(value)
+    except ValueError:
+        return value
+
+    if split_value.scheme and not split_value.netloc and split_value.path:
+        return f"{split_value.scheme}://{split_value.path.lstrip('/')}"
+
     return value
 
 
@@ -62,6 +76,16 @@ def prepend_missing_scheme(value: str, domain_as_url: bool = False) -> str:
     else:
         if not split_value.scheme and "/" in value:
             value = f"https://{value}"
+
+    return value
+
+
+def remove_hidden_unicode_characters(value: str) -> str:
+    # http://www.unicode.org/faq/unsup_char.html
+    hidden_characters = ["\u200c", "\u200d", "\u200e", "\u00ad", "\u2060", "\ufeff", "\u200b", "\u2061", "\u115f"]
+
+    for hidden_character in hidden_characters:
+        value = value.replace(hidden_character, "")
 
     return value
 
